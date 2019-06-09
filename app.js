643d03f8996911bc9d98e3bb240e0e75cb62eb26
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-06-09 23:31:32 +0100

    Callback Chaining.

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-geocode("London", (error, data) => {
-  console.log("Error:", error);
-  console.log("Data:", data);
-});
-
-forecast(-75.7088, 44.1545, (error, data) => {
-  console.log("Error:", error);
-  console.log("Data:", data);
-});
+const location = process.argv[2];
+if (location) {
+  geocode(location, (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(data.location);
+      console.log(forecastData);
+    });
+  });
+} else {
+  console.log("Please provide a location.");
+}
